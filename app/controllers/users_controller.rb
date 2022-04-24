8645f0d41f7fcf08088a6f8b5be26e07bf25b126
Megha <46729920+Megha-Avee@users.noreply.github.com>
2022-04-24 17:49:21 -0500

    added user's privacy checks (#3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :respect_users_privacy, only: %i[ feed discover ]
 
   private
 
@@ -8,6 +9,14 @@ class UsersController < ApplicationController
         @user = User.find_by!(username: params.fetch(:username))
       else
         @user = current_user
+      end
+    end
+
+    def respect_users_privacy
+      if params[:username] != nil
+        if params[:username] != current_user.username
+          redirect_to root_path, alert: "Woah! Let's respect other's privacy!"
+        end
       end
     end
 end
